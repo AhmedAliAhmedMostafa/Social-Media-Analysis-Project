cb903a62e0a184ceba232c21d34ec0674d64d39c
MostafaMohsen010 <36477495+MostafaMohsen010@users.noreply.github.com>
2019-06-29 21:22:06 +0200

    Update Dijkstra_test.cpp

--- Algorithms/closness_centrality/Dijkstra_test.cpp
+++ Algorithms/closness_centrality/Dijkstra_test.cpp
@@ -1,33 +1,23 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 # define INF 0x3f3f3f3f 
-  
 // This class represents a directed graph using  
 // adjacency list representation 
 class Graph 
 { 
     int V;    // No. of vertices 
-    
-    // In a weighted graph, we need to store vertex  
-    // and weight pair for every edge 
     list< pair<int, int> > *adj; 
-  
 public: 
-    Graph(int V);  // Constructor 
-  
+    Graph(int V);  // Constructor  
     // function to add an edge to graph 
     void addEdge(int u, int v, int w); 
-  
     // prints shortest path from s 
     void shortestPath(int s);
-    
-}; 
-  
+};  
 // Allocates memory for adjacency list 
 Graph::Graph(int V) 
 { 
     this->V = V; 
-    
     adj = new list< pair<int, int> >[V]; 
 } 
   
@@ -44,7 +34,6 @@ void Graph::shortestPath(int src)
     // prerocessed 
     set< pair<int, int> > setds; 
   
-    // Create a vector for distances and initialize all 
     // distances as infinite (INF) 
     vector<int> dist(V, INF); 
   
@@ -53,12 +42,8 @@ void Graph::shortestPath(int src)
     setds.insert(make_pair(0, src)); 
     dist[src] = 0; 
   
-    /* Looping till all shortest distance are finalized 
-       then setds will become empty */
     while (!setds.empty()) 
     { 
-        // The first vertex in Set is the minimum distance 
-        // vertex, extract it from set. 
         pair<int, int> tmp = *(setds.begin()); 
         setds.erase(setds.begin()); 
   
@@ -80,12 +65,6 @@ void Graph::shortestPath(int src)
             //  If there is shorter path to v through u. 
             if (dist[v] > dist[u] + weight) 
             { 
-                /*  If distance of v is not INF then it must be in 
-                    our set, so removing it and inserting again 
-                    with updated less distance.   
-                    Note : We extract only those vertices from Set 
-                    for which distance is finalized. So for them,  
-                    we would never reach here.  */
                 if (dist[v] != INF) 
                     setds.erase(setds.find(make_pair(dist[v], v))); 
   
@@ -95,15 +74,11 @@ void Graph::shortestPath(int src)
             } 
         } 
     } 
-  
-    // Print shortest distances stored in dist[]
     double cntral_sum = 0.0; 
-    //printf("Vertex   Distance from Source\n"); 
     for (int i = 0; i < V; ++i) 
         {
             if(dist[i]!=0)
                 cntral_sum += dist[i];
-            //printf("%d \t\t %d\n", i, dist[i]); 
         }
     //printf("closeness centrality from src[ %d ] = %f\n",src,(V-1.0)/cntral_sum);
     cout<<std::setprecision(12)<<(V-1.0)/cntral_sum<<"\n";
@@ -120,20 +95,11 @@ int main()
         cin>>src>>dist>>weight;
         g.addEdge(src,dist,weight);
     }
-    //  making above shown graph 
-    // g.addEdge(0, 1, 1); 
-    // g.addEdge(0, 2, 10); 
-    // g.addEdge(0, 3, 5); 
-    // g.addEdge(2, 3, 4); 
-    // g.addEdge(1, 3, 1); 
-    //g.addEdge(3, 4, 1); 
     
     for(int i=0;i<V;i++)
     {
         //cout<<"======== closeness centrality from src ["<<i<<"] =============\n";
         g.shortestPath(i); 
     }
-        
-  
     return 0; 
 }
