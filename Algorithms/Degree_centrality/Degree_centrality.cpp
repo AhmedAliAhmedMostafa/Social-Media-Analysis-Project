784fd3f770eff87298e0ad530b31be2286b6e1cb
AhmedAliAhmedMostafa <ahmed_elady42@yahoo.com>
2019-07-01 14:14:02 +0200

    add Degree sentrality algorithm implementation

---
+++ Algorithms/Degree_centrality/Degree_centrality.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m,id_1,id_2,weight,max=0,id_max;
+	cin>>n>>m;
+	long long int* counter=new long long int[n];
+	for(int j=0;j<n;j++)
+	{
+		counter[j]=0;
+	}
+	for(int i=0;i<m;i++)
+	{
+		cin>>id_1>>id_2>>weight;
+		counter[id_1]++;
+		counter[id_2]++;
+		if(counter[id_1]>max)
+		{
+			max=counter[id_1];
+			id_max=id_1;
+		}
+		else if(counter[id_2]>max)
+		{
+				max=counter[id_2];
+				id_max=id_2;
+		}
+
+
+	}
+	for(int j=0;j<n;j++)
+	{
+		cout<<counter[j]<<"\n";
+	}
+	return 0;
+}
