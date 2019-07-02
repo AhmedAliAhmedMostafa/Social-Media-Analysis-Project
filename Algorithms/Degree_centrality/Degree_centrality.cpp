53b2aa112ec70d193fc005bafddceabcddac1267
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-07-03 00:14:56 +0200

    Format and Lint Degree_centrality/Degree_centrality.cpp

--- Algorithms/Degree_centrality/Degree_centrality.cpp
+++ Algorithms/Degree_centrality/Degree_centrality.cpp
@@ -1,35 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-	int n,m,id_1,id_2,weight,max=0,id_max;
-	cin>>n>>m;
-	long long int* counter=new long long int[n];
-	for(int j=0;j<n;j++)
+	int n, m, id_1, id_2, weight, max = 0, id_max;
+	cin >> n >> m;
+	long long int *counter = new long long int[n];
+	for (int j = 0; j < n; j++)
 	{
-		counter[j]=0;
+		counter[j] = 0;
 	}
-	for(int i=0;i<m;i++)
+	for (int i = 0; i < m; i++)
 	{
-		cin>>id_1>>id_2>>weight;
+		cin >> id_1 >> id_2 >> weight;
 		counter[id_1]++;
 		counter[id_2]++;
-		if(counter[id_1]>max)
+		if (counter[id_1] > max)
 		{
-			max=counter[id_1];
-			id_max=id_1;
+			max = counter[id_1];
+			id_max = id_1;
 		}
-		else if(counter[id_2]>max)
+		else if (counter[id_2] > max)
 		{
-				max=counter[id_2];
-				id_max=id_2;
+			max = counter[id_2];
+			id_max = id_2;
 		}
-
-
 	}
-	for(int j=0;j<n;j++)
+	for (int j = 0; j < n; j++)
 	{
-		cout<<counter[j]<<"\n";
+		cout << counter[j] << "\n";
 	}
 	return 0;
 }
